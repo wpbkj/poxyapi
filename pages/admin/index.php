fde427a2adde4784ef9163fbd6f5388e759572b6
wpbkj <wpbkj123@163.com>
2022-06-15 22:07:11 +0800

    第二次条件SVG|ADMIN

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -1,2 +1,116 @@
-<?php
-echo POXYAPI_Public_URL('css/bootstrap-grid.min.css');
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>无为API管控系统后台</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="<?php echo POXYAPI_Public_URL('css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="<?php echo POXYAPI_Public_URL('css/dashboard.css') ?>" rel="stylesheet">
+  </head>
+  <body>
+    
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">POXYAPI ADMIN</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="搜索内容" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">登出系统</a>
+    </div>
+  </div>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="?core=index&page=index">
+            <img src="<?php echo POXYAPI_Public_URL('icons/house-door.svg') ?>" alt="house-door" width="auto" height="auto">
+              首页概述
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/gear.svg') ?>" alt="gear" width="auto" height="auto">
+              基本设置
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/gear-wide-connected.svg') ?>" alt="gear-wide-connected" width="auto" height="auto">
+              系统设置
+            </a>
+          </li>
+          <hr>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/sliders.svg') ?>" alt="sliders" width="auto" height="auto">
+              API管理
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/layers.svg') ?>" alt="layers" width="auto" height="auto">
+              API数据分析
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/stickies.svg') ?>" alt="stickies" width="auto" height="auto">
+              前端页面生成
+            </a>
+          </li>
+          <hr>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="<?php echo POXYAPI_Public_URL('icons/people-fill.svg') ?>" alt="people-fill" width="auto" height="auto">
+              管理员设置
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">首页概述</h1>
+        
+      </div>
+    </main>
+  </div>
+</div>
+
+
+    <script src="<?php echo POXYAPI_Public_URL('js/bootstrap.bundle.min.js') ?>"></script>
+
+      <script src="<?php echo POXYAPI_Public_URL('js/feather.min.js') ?>" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+  </body>
+</html>
